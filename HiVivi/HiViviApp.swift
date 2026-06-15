2c8f98ecafc1e4bf3193d8a90633fbabaaa49f5b
zzy <2218847957@qq.com>
2026-06-15 18:31:29 +0800

    Initial Commit

---
+++ HiVivi/HiViviApp.swift
@@ -0,0 +1,17 @@
+//
+//  HiViviApp.swift
+//  HiVivi
+//
+//  Created by yangyang on 2026/6/15.
+//
+
+import SwiftUI
+
+@main
+struct HiViviApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
